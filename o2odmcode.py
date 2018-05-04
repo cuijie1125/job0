5220c24b23c4ab1fc831bb25411625c778c5182d
cuijie1125 <37904960+cuijie1125@users.noreply.github.com>
2018-05-04 11:38:12 +0800

    Add files via upload

---
+++ o2odmcode.py
@@ -0,0 +1,313 @@
+﻿
+import os, sys, pickle
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import seaborn as sns
+
+from datetime import date
+
+from sklearn.model_selection import KFold, train_test_split, StratifiedKFold, cross_val_score, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import log_loss, roc_auc_score, auc, roc_curve
+from sklearn.preprocessing import MinMaxScaler
+
+import xgboost as xgb
+import lightgbm as lgb 
+
+
+#dfoff = pd.read_csv('data/ccf_offline_stage1_train.csv')
+dfoff = pd.read_csv('data/ccf_offline_stage1_train.csv',dtype={'Date_received': np.str,'Date':np.str,'Coupon_id':np.str,'Distance':np.str})
+dftest = pd.read_csv('data/ccf_offline_stage1_test_revised.csv',dtype={'Distance': np.str})
+
+dfon = pd.read_csv('data/ccf_online_stage1_train.csv')
+
+dfoff['Date_received']=dfoff['Date_received'].fillna('null')
+dfoff['Date']=dfoff['Date'].fillna('null')
+
+dfoff.head(5)
+dfoff.info()
+
+def usermerchantFeature(df):
+
+    um = df[['User_id', 'Merchant_id']].copy().drop_duplicates()
+
+    um1 = df[['User_id', 'Merchant_id']].copy()
+    um1['um_count'] = 1
+    um1 = um1.groupby(['User_id', 'Merchant_id'], as_index = False).count()
+
+    um2 = df[df['Date'] != 'null'][['User_id', 'Merchant_id']].copy()
+    um2['um_buy_count'] = 1
+    um2 = um2.groupby(['User_id', 'Merchant_id'], as_index = False).count()
+
+    um3 = df[df['Date_received'] != 'null'][['User_id', 'Merchant_id']].copy()
+    um3['um_coupon_count'] = 1
+    um3 = um3.groupby(['User_id', 'Merchant_id'], as_index = False).count()
+
+    um4 = df[(df['Date_received'] != 'null') & (df['Date'] != 'null')][['User_id', 'Merchant_id']].copy()
+    um4['um_buy_with_coupon'] = 1
+    um4 = um4.groupby(['User_id', 'Merchant_id'], as_index = False).count()
+
+    user_merchant_feature = pd.merge(um, um1, on = ['User_id','Merchant_id'], how = 'left')
+    user_merchant_feature = pd.merge(user_merchant_feature, um2, on = ['User_id','Merchant_id'], how = 'left')
+    user_merchant_feature = pd.merge(user_merchant_feature, um3, on = ['User_id','Merchant_id'], how = 'left')
+    user_merchant_feature = pd.merge(user_merchant_feature, um4, on = ['User_id','Merchant_id'], how = 'left')
+    user_merchant_feature = user_merchant_feature.fillna(0)
+
+    user_merchant_feature['um_buy_rate'] = user_merchant_feature['um_buy_count'].astype('float')/user_merchant_feature['um_count'].astype('float')
+    user_merchant_feature['um_coupon_use_rate'] = user_merchant_feature['um_buy_with_coupon'].astype('float')/user_merchant_feature['um_coupon_count'].astype('float')
+    user_merchant_feature['um_buy_with_coupon_rate'] = user_merchant_feature['um_buy_with_coupon'].astype('float')/user_merchant_feature['um_buy_count'].astype('float')
+    user_merchant_feature = user_merchant_feature.fillna(0)
+
+    print(user_merchant_feature.columns.tolist())
+    return user_merchant_feature
+def userFeature(df):
+    u = df[['User_id']].copy().drop_duplicates()
+    # u_coupon_count : num of coupon received by user
+    u1 = df[df['Date_received'] != 'null'][['User_id']].copy()
+    u1['u_coupon_count'] = 1
+    u1 = u1.groupby(['User_id'], as_index = False).count()
+
+    # u_buy_count : times of user buy offline (with or without coupon)
+    u2 = df[df['Date'] != 'null'][['User_id']].copy()
+    u2['u_buy_count'] = 1
+    u2 = u2.groupby(['User_id'], as_index = False).count()
+
+    # u_buy_with_coupon : times of user buy offline (with coupon)
+    u3 = df[((df['Date'] != 'null') & (df['Date_received'] != 'null'))][['User_id']].copy()
+    u3['u_buy_with_coupon'] = 1
+    u3 = u3.groupby(['User_id'], as_index = False).count()
+
+    # u_merchant_count : num of merchant user bought from
+    u4 = df[df['Date'] != 'null'][['User_id', 'Merchant_id']].copy()
+    u4.drop_duplicates(inplace = True)
+    u4 = u4.groupby(['User_id'], as_index = False).count()
+    u4.rename(columns = {'Merchant_id':'u_merchant_count'}, inplace = True)
+
+    # u_min_distance
+    utmp = df[(df['Date'] != 'null') & (df['Date_received'] != 'null')][['User_id', 'distance']].copy()
+    utmp.replace(-1, np.nan, inplace = True)
+       #I add
+    #utmp['Distance']=utmp['Distance'].fillna(0)
+   # utmp['Distance']=utmp['Distance'].astype(int)
+    
+    
+    u5 = utmp.groupby(['User_id'], as_index = False).min()
+    u5.rename(columns = {'distance':'u_min_distance'}, inplace = True)
+    u6 = utmp.groupby(['User_id'], as_index = False).max()
+    u6.rename(columns = {'distance':'u_max_distance'}, inplace = True)
+    u7 = utmp.groupby(['User_id'], as_index = False).mean()
+    u7.rename(columns = {'distance':'u_mean_distance'}, inplace = True)
+    u8 = utmp.groupby(['User_id'], as_index = False).median()
+    u8.rename(columns = {'distance':'u_median_distance'}, inplace = True)
+
+    user_feature = pd.merge(u, u1, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u2, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u3, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u4, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u5, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u6, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u7, on = 'User_id', how = 'left')
+    user_feature = pd.merge(user_feature, u8, on = 'User_id', how = 'left')
+
+    user_feature['u_use_coupon_rate'] = user_feature['u_buy_with_coupon'].astype('float')/user_feature['u_coupon_count'].astype('float')
+    user_feature['u_buy_with_coupon_rate'] = user_feature['u_buy_with_coupon'].astype('float')/user_feature['u_buy_count'].astype('float')
+    user_feature = user_feature.fillna(0)
+    
+    print(user_feature.columns.tolist())
+    return user_feature
+def merchantFeature(df):
+    m = df[['Merchant_id']].copy().drop_duplicates()
+
+    # m_coupon_count : num of coupon from merchant
+    m1 = df[df['Date_received'] != 'null'][['Merchant_id']].copy()
+    m1['m_coupon_count'] = 1
+    m1 = m1.groupby(['Merchant_id'], as_index = False).count()
+
+    # m_sale_count : num of sale from merchant (with or without coupon)
+    m2 = df[df['Date'] != 'null'][['Merchant_id']].copy()
+    m2['m_sale_count'] = 1
+    m2 = m2.groupby(['Merchant_id'], as_index = False).count()
+
+    # m_sale_with_coupon : num of sale from merchant with coupon usage
+    m3 = df[(df['Date'] != 'null') & (df['Date_received'] != 'null')][['Merchant_id']].copy()
+    m3['m_sale_with_coupon'] = 1
+    m3 = m3.groupby(['Merchant_id'], as_index = False).count()
+
+    # m_min_distance
+    mtmp = df[(df['Date'] != 'null') & (df['Date_received'] != 'null')][['Merchant_id', 'distance']].copy()
+    mtmp.replace(-1, np.nan, inplace = True)
+    # I add
+    #mtmp['Distance']=mtmp['Distance'].fillna(0)
+    #mtmp['Distance']=mtmp['Distance'].astype(int)
+        
+    m4 = mtmp.groupby(['Merchant_id'], as_index = False).min()
+    m4.rename(columns = {'distance':'m_min_distance'}, inplace = True)
+    m5 = mtmp.groupby(['Merchant_id'], as_index = False).max()
+    m5.rename(columns = {'distance':'m_max_distance'}, inplace = True)
+    m6 = mtmp.groupby(['Merchant_id'], as_index = False).mean()
+    m6.rename(columns = {'distance':'m_mean_distance'}, inplace = True)
+    m7 = mtmp.groupby(['Merchant_id'], as_index = False).median()
+    m7.rename(columns = {'distance':'m_median_distance'}, inplace = True)
+
+    merchant_feature = pd.merge(m, m1, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m2, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m3, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m4, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m5, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m6, on = 'Merchant_id', how = 'left')
+    merchant_feature = pd.merge(merchant_feature, m7, on = 'Merchant_id', how = 'left')
+
+    merchant_feature['m_coupon_use_rate'] = merchant_feature['m_sale_with_coupon'].astype('float')/merchant_feature['m_coupon_count'].astype('float')
+    merchant_feature['m_sale_with_coupon_rate'] = merchant_feature['m_sale_with_coupon'].astype('float')/merchant_feature['m_sale_count'].astype('float')
+    merchant_feature = merchant_feature.fillna(0)
+    
+    print(merchant_feature.columns.tolist())
+    return merchant_feature
+#数据标注
+def label(row):
+    if row['Date_received'] == 'null':
+        return -1
+    if row['Date'] != 'null':
+        td = pd.to_datetime(row['Date'], format='%Y%m%d') -  pd.to_datetime(row['Date_received'], format='%Y%m%d')
+        if td <= pd.Timedelta(15, 'D'):
+            return 1
+    return 0
+dfoff['label'] = dfoff.apply(label, axis = 1)
+print(dfoff['label'].value_counts())
+
+#
+def getDiscountType(row):
+    if row == 'null':
+        return 'null'
+    elif ':' in row:
+        return 1
+    else:
+        return 0
+
+def convertRate(row):
+    """Convert discount to rate"""
+    if row == 'null':
+        return 1.0
+    elif ':' in row:
+        rows = row.split(':')
+        return 1.0 - float(rows[1])/float(rows[0])
+    else:
+        return float(row)
+
+def getDiscountMan(row):
+    if ':' in row:
+        rows = row.split(':')
+        return int(rows[0])
+    else:
+        return 0
+
+def getDiscountJian(row):
+    if ':' in row:
+        rows = row.split(':')
+        return int(rows[1])
+    else:
+        return 0
+
+def processData(df):
+    
+    # convert discunt_rate
+    df['discount_rate'] = df['Discount_rate'].apply(convertRate)
+    df['discount_man'] = df['Discount_rate'].apply(getDiscountMan)
+    df['discount_jian'] = df['Discount_rate'].apply(getDiscountJian)
+    df['discount_type'] = df['Discount_rate'].apply(getDiscountType)
+    print(df['discount_rate'].unique())
+    
+    # convert distance
+       
+    df['distance'] = df['Distance'].replace('null', -1).astype(int)
+    print(df['distance'].unique())
+    return df
+#I add
+dfoff['Discount_rate']=dfoff['Discount_rate'].fillna('null')
+dfoff['Distance']=dfoff['Distance'].fillna('null')
+dftest['Distance']=dftest['Distance'].fillna('null')
+dftest['Discount_rate']=dftest['Discount_rate'].fillna('null')
+
+dfoff = processData(dfoff)
+dftest = processData(dftest)
+##########
+def featureProcess(feature, train, test):
+    """
+    feature engineering from feature data
+    then assign user, merchant, and user_merchant feature for train and test 
+    """
+    
+    user_feature = userFeature(feature)
+    merchant_feature = merchantFeature(feature)
+    user_merchant_feature = usermerchantFeature(feature)
+    
+    train = pd.merge(train, user_feature, on = 'User_id', how = 'left')
+    train = pd.merge(train, merchant_feature, on = 'Merchant_id', how = 'left')
+    train = pd.merge(train, user_merchant_feature, on = ['User_id', 'Merchant_id'], how = 'left')
+    train = train.fillna(0)
+    
+    test = pd.merge(test, user_feature, on = 'User_id', how = 'left')
+    test = pd.merge(test, merchant_feature, on = 'Merchant_id', how = 'left')
+    test = pd.merge(test, user_merchant_feature, on = ['User_id', 'Merchant_id'], how = 'left')
+    test = test.fillna(0)
+    
+    return train, test
+# repeat result above and process dftest data
+# features
+predictors = ['discount_rate', 'discount_man', 'discount_jian', 'discount_type', 'distance',
+              'u_coupon_count', 'u_buy_count', 'u_buy_with_coupon', 'u_merchant_count', 'u_min_distance', 
+              'u_max_distance', 'u_mean_distance', 'u_median_distance', 'u_use_coupon_rate', 'u_buy_with_coupon_rate', 
+              'm_coupon_count', 'm_sale_count', 'm_sale_with_coupon', 'm_min_distance', 'm_max_distance',
+              'm_mean_distance', 'm_median_distance', 'm_coupon_use_rate', 'm_sale_with_coupon_rate', 'um_count', 'um_buy_count', 
+              'um_coupon_count', 'um_buy_with_coupon', 'um_buy_rate', 'um_coupon_use_rate', 'um_buy_with_coupon_rate']
+print(len(predictors), predictors)
+feature = dfoff[(dfoff['Date'] < '20160516') | ((dfoff['Date'] == 'null') & (dfoff['Date_received'] < '20160516'))].copy()
+data = dfoff[(dfoff['Date_received'] >= '20160516') & (dfoff['Date_received'] <= '20160615')].copy()
+print(data['label'].value_counts())
+
+# feature engineering
+train, test = featureProcess(feature, data, dftest)
+
+############dm
+trainSub,validSub=train_test_split(train,test_size =0.2,stratify=train['label'],random_state=100)
+model=lgb.LGBMClassifier(
+                    learning_rate = 0.01,
+                    boosting_type = 'gbdt',
+                    objective = 'binary',
+                    metric = 'logloss',
+                    max_depth = 5,
+                    sub_feature = 0.7,
+                    num_leaves = 3,
+                    colsample_bytree = 0.7,
+                    n_estimators = 5000,
+                    early_stop = 50,
+                    verbose = -1)
+model.fit(trainSub[predictors],trainSub['label'])
+###auc
+validSub['pred_prob'] = model.predict_proba(validSub[predictors])[:,1]
+validgroup = validSub.groupby(['Coupon_id'])
+aucs = []
+for i in validgroup:
+    tmpdf = i[1] 
+    if len(tmpdf['label'].unique()) != 2:
+        continue
+    fpr, tpr, thresholds = roc_curve(tmpdf['label'], tmpdf['pred_prob'], pos_label=1)
+    aucs.append(auc(fpr, tpr))
+print(np.average(aucs))
+
+# test prediction for submission
+y_test_pred = model.predict_proba(test[predictors])
+submit = test[['User_id','Coupon_id','Date_received']].copy()
+submit['label'] = y_test_pred[:,1]
+submit.to_csv('submit_gbm.csv', index=False, header=False)
+submit.head()      
+
+
+
